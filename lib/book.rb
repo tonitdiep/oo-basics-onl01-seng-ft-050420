7d2c0140f45549c236437fa9ef9ade46c7e1e6d2
Toni  Diep <adiep123@gmail.com>
2020-05-08 04:07:06 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,55 @@
+# class Book
 
+# def initialize(title)
+#   @title = title
+#   end
 
+# def title   #getter gets the property for us 
+#   @title
+# end
+# #now need setter, set instance variables property= author , instance variable @author
+# def author=(author)
+#   @author = author
+# end
+# #setting the author method below (the "getter"):
+# def author
+#   @author
+# end
+# #need both setter and getting for page_count
+
+# def page_count=(num)
+#   @page_count = num
+# end
+
+# def page_count
+#   @page_count
+# end
+
+# def genre=(genre)
+#   @genre = genre 
+# end
+
+# def genre
+#   @genre
+# end
+
+# def turn_page
+#   puts "Flipping the page...wow, you read fast!"
+# end
+# end
+
+#or 
+
+class Book 
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  def initialize(title)
+    @title = title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+# Book.new("And Then There Were None")
+# book = Book.new("Some Title")
+# book.turn_page
